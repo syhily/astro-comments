4b0d260c66e39ac68c89bdccbfee369925192f4a
Yufan Sheng <syhily@gmail.com>
2025-01-10 17:05:35 +0800

    feat: add the schema tables for better auth.

--- db/migration/V_1_create_auth_tables.ts
+++ db/migration/V_1_create_auth_tables.ts
@@ -1,9 +1,52 @@
 import type { DbMigrator } from '../../types';
 
 const migrate: DbMigrator = {
-  statements: [],
+  statements: [
+    `CREATE TABLE "hc_user" (
+  "id" TEXT NOT NULL PRIMARY KEY,
+  "name" TEXT NOT NULL,
+  "email" TEXT NOT NULL UNIQUE,
+  "email_verified" INTEGER NOT NULL,
+  "image" TEXT,
+  "created_at" INTEGER NOT NULL,
+  "updated_at" INTEGER NOT NULL
+);`,
+    `CREATE TABLE "hc_session" (
+  "id" TEXT NOT NULL PRIMARY KEY,
+  "expires_at" INTEGER NOT NULL,
+  "token" TEXT NOT NULL UNIQUE,
+  "created_at" INTEGER NOT NULL,
+  "updated_at" INTEGER NOT NULL,
+  "ip_address" TEXT,
+  "user_agent" TEXT,
+  "user_id" TEXT NOT NULL
+);`,
+    `CREATE TABLE "hc_account" (
+  "id" TEXT NOT NULL PRIMARY KEY,
+  "account_id" TEXT NOT NULL,
+  "provider_id" TEXT NOT NULL,
+  "user_id" TEXT NOT NULL,
+  "access_token" TEXT,
+  "refresh_token" TEXT,
+  "id_token" TEXT,
+  "access_token_expires_at" INTEGER,
+  "refresh_token_expires_at" INTEGER,
+  "scope" TEXT,
+  "password" TEXT,
+  "created_at" INTEGER NOT NULL,
+  "updated_at" INTEGER NOT NULL
+);`,
+    `CREATE TABLE "hc_verification" (
+  "id" TEXT NOT NULL PRIMARY KEY,
+  "identifier" TEXT NOT NULL,
+  "value" TEXT NOT NULL,
+  "expires_At" INTEGER NOT NULL,
+  "created_at" INTEGER NOT NULL,
+  "updated_at" INTEGER NOT NULL
+);`,
+  ],
   version: 1,
-  changelog: ['Create the auth table for storing the credentials.'],
+  changelog: ['Create the better auth table for storing the credentials.'],
 };
 
 export default migrate;
